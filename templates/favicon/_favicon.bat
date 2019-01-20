663ddc9a2b543890514f6b0c8bba9bca5eddafb5
Carlos Pardo <carlos@picuino.com>
2019-01-20 16:38:12 +0100

    favicon.ico with white background-color. ADD .htaccess

---
+++ templates/favicon/_favicon.bat
@@ -0,0 +1,2 @@
+set convert=/Bin/imagemagick/convert.exe 
+for %%f in (*.png) do %convert%  %%f -resize 32x32  %%f.ico
--- templates/tools/_favicon.bat
+++
@@ -1,2 +0,0 @@
-set convert=/Bin/imagemagick/convert.exe 
-%convert%  %1  -resize 32x32  favicon.ico
